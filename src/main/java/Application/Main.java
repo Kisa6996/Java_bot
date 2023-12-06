8dbd8b20af7d204c6314d13d93bdd7a237cbdc9f
Никита Рудь <nikitarud@MacBook-Air-Nikita.local>
2023-12-06 16:51:41 +1000

    Добваил кнопки и отправку сообщений

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -1,7 +1,16 @@
 package Application;
 
+import java.io.IOException;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import Telegram.BotMenu;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws TelegramApiException, IOException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(new BotMenu());
     }
 }
